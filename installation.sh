0cf3bd036767de3bca68012c5248cdfdf731a545
faika <faika@gmail.com>
2016-04-05 18:48:50 +0100

    installation

--- installation.sh
+++ installation.sh
@@ -5,5 +5,7 @@ echo "upgrade"
 sudo apt-get upgrade -y  > /dev/null 2>&1
 echo "installation redis server"
 sudo apt-get -y install -y redis-server > /dev/null 2>&1
+echo "Démarrer Redis....:"
+sudo service redis-server start
 echo "Etat:"
 sudo service redis-server status
