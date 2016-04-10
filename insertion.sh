1819afce8db42c46bb5fd25ac1bb5177b9940f1c
faika <faika@gmail.com>
2016-04-10 20:52:43 +0100

    installation et insertion depuis le fichier

--- insertion.sh
+++ insertion.sh
@@ -1,13 +1,26 @@
-#echo "insertion de données statiques dans la base de données Redis...."
-#echo "SET profiles:BrandonAmos2  \"first : brandon, last: amos, phone:  ~(540)~947~1238, email: bamos@cs.cmu.edu, pdf: /data/cv.pdf ,src :  www.github.com/bamos/cv , url : bamos.github.io , github : bamos , twitter :brandonamos , linkedin :bdamos\"" | redis-cli
-#echo "affichage des données..."
-#echo "GET profiles:BrandonAmos2" | redis-cli
+
+echo "update.."
+sudo apt-get update -y > /dev/null 2>&1
+echo "upgrade"
+sudo apt-get upgrade -y  > /dev/null 2>&1
+echo "installation redis server"
+sudo apt-get -y install redis-server > /dev/null 2>&1
+echo "Démarrer Redis....:"
+sudo service redis-server start
+echo "Etat:"
+sudo service redis-server status
+
+echo "insertion de données statiques dans la base de données Redis...."
+echo "SET profiles:BrandonAmos2  \"first : brandon, last: amos, phone:  ~(540)~947~1238, email: bamos@cs.cmu.edu, pdf: /data/cv.pdf ,src :  www.github.com/bamos/cv , url : bamos.github.io , github : bamos , twitter :brandonamos , linkedin :bdamos\"" | redis-cli
+echo "affichage des données..."
+echo "GET profiles:BrandonAmos2" | redis-cli
+
 echo "insertion de données depuis le fichier yaml dans la base de données Redis...."
 
 FILE=`cat informations.yml`
 tab=( $( awk -F ":" '{print $1}' informations.yml) )
 tab2=( $( awk -F ":" '{print $2}' informations.yml) )
-chaine="SET profiles:FaikaBouProfile \""
+chaine="SET profiles:tableProfile \""
 
 length=${#tab[@]}
 i=0
@@ -22,5 +35,5 @@ echo "$chaine"
 echo "$chaine "  | redis-cli
 echo "affichage des données depuis la base de données...\n"
 
-echo "GET profiles:FaikaBouProfile " | redis-cli
+echo "GET profiles:tableProfile " | redis-cli
 
